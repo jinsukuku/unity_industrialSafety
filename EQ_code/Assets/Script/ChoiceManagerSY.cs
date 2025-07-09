1b0122633063c053e3fee798f481d4edcf0b2e28
Soso-Kiki <polytechid10@gmail.com>
2025-07-09 15:21:22 +0900

    feat:SceneCh02에 분리했던 씬 내용 합치기 + 책상밑영상 추가

--- EQ_code/Assets/Script/ChoiceManagerSY.cs
+++ EQ_code/Assets/Script/ChoiceManagerSY.cs
@@ -3,9 +3,13 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using static UnityEngine.EventSystems.EventTrigger;
+using UnityEngine.Video;
 
 public class ChoiceManager : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;          // VideoPlayer 연결
+    public VideoClip nextVideoClip;          // 바꿔줄 '책상 아래' 영상
+
     // 선택지 버튼
     public Button optionA;
     public Button optionB;
@@ -13,6 +17,10 @@ public class ChoiceManager : MonoBehaviour
 
     // "다시 선택하기" 버튼
     public Button retryButton;
+
+    // "NEXT" 버튼
+    public Button nextButton;
+    private bool isNextStage = false; // NEXT를 한 번 눌렀는지 여부
 
     // 대화 텍스트 
     public Text dialogueText;
@@ -67,7 +75,17 @@ public class ChoiceManager : MonoBehaviour
 
         if (option == "B")
         {
-            SceneManager.LoadScene("SceneCh01"); // 씬 1개로 변경 예정이라 임시로 수정해 놓음 
+            // SceneManager.LoadScene("SceneCh01"); // 씬이동 > 말고 한 씬으로 제작하기
+
+            dialoguePanel.SetActive(true);
+            optionPanel.SetActive(false);
+            retryButton.gameObject.SetActive(false); // 다시선택 숨기기
+            nextButton.gameObject.SetActive(true);   // NEXT 버튼 표시
+            dialogueText.text = "정답이야! 이제 실제로 튼튼한 책상 밑으로 들어보자.";
+
+            // NEXT 버튼 클릭 시 탁자아래 연결
+            nextButton.onClick.RemoveAllListeners(); // 전에 쌓인 이벤트 리스너 제거
+            nextButton.onClick.AddListener(OnNextClicked); // 버튼 클릭 시 실행할 동작 새로 지정
         }
         else if ((option == "A" && !aSelected) || (option == "C" && !cSelected))
         {
@@ -98,5 +116,32 @@ public class ChoiceManager : MonoBehaviour
         optionPanel.SetActive(true);
         dialogueText.text = "다른 선택지를 골라봐.";
     }
+
+    void OnNextClicked()
+    {
+
+        if (!isNextStage)
+        {
+            // 텍스트만 변경
+            dialogueText.text = "진동이 잦아질 때까지 기다립니다.\n진동이 잦아들면 탁자에서 나와 안전을 확보합니다.";
+            isNextStage = true; // 다음 클릭에서 씬 전환하도록 표시
+
+            // 영상 전환 > 책상 아래
+            if (videoPlayer != null && nextVideoClip != null)
+            {
+                videoPlayer.Stop();
+                videoPlayer.clip = nextVideoClip;
+                videoPlayer.Play();
+            }
+
+        }
+        else
+        {
+            // 두 번째 NEXT 클릭시, 씬 이동
+            SceneManager.LoadScene("SceneIntro");
+        }
+
+    }
+
 
 }
